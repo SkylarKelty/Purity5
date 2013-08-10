73948014d3e16cf7e407b0ed8bb91d1a81f14dba
Skylar Kelty <skylarkelty@gmail.com>
2013-08-10 11:23:05 +0100

    Add example usage

--- src/SkylarK/Purity5/Purity5.php
+++ src/SkylarK/Purity5/Purity5.php
@@ -9,6 +9,12 @@ namespace SkylarK\Purity5;
 
 /**
  * The core of Purity5.
+ * Usage:
+ *   $html = Purity5::parse($markup);
+ *   $title = $html("title");
+ *   $table_headings = $html("body table tr th");
+ *   $active_links = $html("body a[class=active]");
+ *   $active_current_links = $html("body a[class=active+current]");
  */
 class Purity5
 {
