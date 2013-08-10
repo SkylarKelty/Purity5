7478e371766bc78a4734d9cdcae78d95b64f0858
Skylar Kelty <skylarkelty@gmail.com>
2013-08-10 11:20:05 +0100

    Add base class

---
+++ src/SkylarK/Purity5/Purity5.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Purity5 is a HTML5 parser
+ *
+ * @author Skylar Kelty <skylarkelty@gmail.com>
+ */
+
+namespace SkylarK\Purity5;
+
+/**
+ * The core of Purity5.
+ */
+class Purity5
+{
+}
