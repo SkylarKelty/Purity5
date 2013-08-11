0601de66ee205fc7f62a7222b76e8207b7cacf1f
Skylar Kelty <skylarkelty@gmail.com>
2013-08-11 10:49:24 +0100

    Add in more query tests

--- tests/SkylarK/Purity5/DataStructures/PureTreeTest.php
+++ tests/SkylarK/Purity5/DataStructures/PureTreeTest.php
@@ -22,30 +22,49 @@ class PureTreeTest extends PHPUnit_Framework_TestCase
 		$this->assertEquals(1, count($root->children()));
 		$this->assertEquals(1, count($c1->children()));
 		$this->assertEquals(0, count($c2->children()));
+	}
+
+	public function test_TreeQuery() {
+		
+		$root = PureTree::buildRoot('<head><title>Welcome</title></head><body><h1>Lorum!</h1><p>String 1</p><p>String 2</p></body></html');
+		
+		$head = $root->createChild("head", array(), '<title>Welcome</title>');
+		$title = $head->createChild("title", array(), 'Welcome');
+
+		$body = $root->createChild("body", array(), '<h1>Lorum!</h1><p>String 1</p><p>String 2</p>');
+		$h1 = $body->createChild("h1", array(), 'Lorum!');
+		$p1 = $body->createChild("p", array(), 'String 1');
+		$p2 = $body->createChild("p", array(), 'String 2');
 
 		// Now test querying
 		$result = $root->query("title");
 		$this->assertEquals(1, count($result));
-		$this->assertEquals($c2, $result[0]);
+		$this->assertEquals($title, $result[0]);
 
 		// More complex query
 		$result = $root->query("html > head > title");
 		$this->assertEquals(1, count($result));
-		$this->assertEquals($c2, $result[0]);
+		$this->assertEquals($title, $result[0]);
 
 		// Another query
 		$result = $root->query("html head title");
 		$this->assertEquals(1, count($result));
-		$this->assertEquals($c2, $result[0]);
+		$this->assertEquals($title, $result[0]);
 
 		// Another query
 		$result = $root->query("html title");
 		$this->assertEquals(1, count($result));
-		$this->assertEquals($c2, $result[0]);
+		$this->assertEquals($title, $result[0]);
 
 		// Another query
 		$result = $root->query("head > title");
 		$this->assertEquals(1, count($result));
-		$this->assertEquals($c2, $result[0]);
+		$this->assertEquals($title, $result[0]);
+
+		// Multiple result query
+		$result = $root->query("body > p");
+		$this->assertEquals(2, count($result));
+		$this->assertEquals($p1, $result[0]);
+		$this->assertEquals($p2, $result[1]);
 	}
 }
