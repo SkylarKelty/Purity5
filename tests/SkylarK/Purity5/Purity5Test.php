854db46d88b0aaa0f8a409f21eb4608822047a88
Skylar Kelty <skylarkelty@gmail.com>
2013-08-12 14:25:19 +0100

    Tests and utils

--- tests/SkylarK/Purity5/Purity5Test.php
+++ tests/SkylarK/Purity5/Purity5Test.php
@@ -21,5 +21,39 @@ class Purity5Test extends PHPUnit_Framework_TestCase
 		$this->assertEquals("Welcome", $func("head")->title);
 		$this->assertEquals("Welcome to Purity5!", $func("html> body >p"));
 		$this->assertEquals("Welcome to Purity5!", $func("html")->body->p);
+		$this->assertEquals("Welcome", $func("title")->contents());
+	}
+
+	public function test_Utils() {
+		$html = '<html>
+			<head>
+				<title>Welcome</title>
+			</head>
+			<body>
+				<h1>Heading</h1>
+				<p class="welcome" var="true">Welcome to Purity5!</p>
+			</body>
+		</html>';
+		
+		$func = SkylarK\Purity5\Purity5::parse($html);
+		$this->assertEquals("Welcome to Purity5!", $func(".welcome"));
+		$this->assertEquals(array("class" => "welcome", "var" => "true"), $func(".welcome")->attrs());
+		$this->assertEquals("welcome", $func(".welcome")->attr("class"));
+	}
+
+	public function test_Array() {
+		$html = '<html>
+			<head>
+				<title>Welcome</title>
+			</head>
+			<body>
+				<h1>Heading</h1>
+				<p class="welcome" var="true">Welcome to Purity5!</p>
+				<p class="lorum" var="true">Lorum Ipsum</p>
+			</body>
+		</html>';
+		
+		$func = SkylarK\Purity5\Purity5::parse($html);
+		$this->assertEquals(2, count($func("p[var=true]")));
 	}
 }
